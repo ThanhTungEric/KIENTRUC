b8136cb1972294d0a64d43cda4cc6da77dfabdb2
ThanhTungEric <99382836+ThanhTungEric@users.noreply.github.com>
2024-04-20 08:55:56 +0700

    push cham diem thuc hanh

---
+++ TUAN01/springboot-frist-week/src/main/java/com/iuh/SpringbootFristWeekApplication.java
@@ -0,0 +1,13 @@
+package com.iuh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFristWeekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootFristWeekApplication.class, args);
+    }
+
+}
---
+++ TUAN01/springboot-frist-week/src/main/java/com/iuh/model/Person.java
@@ -0,0 +1,35 @@
+package com.iuh.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "person")
+public class Person {
+    @Id
+    private String id;
+    private String name;
+
+    public Person(@JsonProperty("id") String id,
+                  @JsonProperty("name") String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
